d89c62541c7c5c8db10bed2257e8f1fcdabfb5bb
winte <winte@DESKTOP-RF4KC6K>
2023-02-02 00:53:45 +0800

    加入專案檔案。

---
+++ RVITakePic/RVITakePic.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace RVIServer
+{
+    partial class RVITakePic
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lb_UserList = new System.Windows.Forms.ListBox();
+            this.lable1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tb_IP = new System.Windows.Forms.TextBox();
+            this.tb_Port = new System.Windows.Forms.TextBox();
+            this.btn_Restart = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lb_UserList
+            // 
+            this.lb_UserList.FormattingEnabled = true;
+            this.lb_UserList.ItemHeight = 18;
+            this.lb_UserList.Location = new System.Drawing.Point(20, 81);
+            this.lb_UserList.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.lb_UserList.Name = "lb_UserList";
+            this.lb_UserList.Size = new System.Drawing.Size(178, 130);
+            this.lb_UserList.TabIndex = 0;
+            // 
+            // lable1
+            // 
+            this.lable1.AutoSize = true;
+            this.lable1.Location = new System.Drawing.Point(60, 58);
+            this.lable1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lable1.Name = "lable1";
+            this.lable1.Size = new System.Drawing.Size(98, 18);
+            this.lable1.TabIndex = 1;
+            this.lable1.Text = "線上使用者";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(255, 86);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 18);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Server IP";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(258, 144);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(86, 18);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Server Port";
+            // 
+            // tb_IP
+            // 
+            this.tb_IP.Enabled = false;
+            this.tb_IP.Location = new System.Drawing.Point(372, 81);
+            this.tb_IP.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tb_IP.Name = "tb_IP";
+            this.tb_IP.Size = new System.Drawing.Size(148, 29);
+            this.tb_IP.TabIndex = 4;
+            // 
+            // tb_Port
+            // 
+            this.tb_Port.Location = new System.Drawing.Point(372, 144);
+            this.tb_Port.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tb_Port.Name = "tb_Port";
+            this.tb_Port.Size = new System.Drawing.Size(148, 29);
+            this.tb_Port.TabIndex = 5;
+            // 
+            // btn_Restart
+            // 
+            this.btn_Restart.Location = new System.Drawing.Point(372, 204);
+            this.btn_Restart.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btn_Restart.Name = "btn_Restart";
+            this.btn_Restart.Size = new System.Drawing.Size(112, 34);
+            this.btn_Restart.TabIndex = 7;
+            this.btn_Restart.Text = "Restart";
+            this.btn_Restart.UseVisualStyleBackColor = true;
+            this.btn_Restart.Click += new System.EventHandler(this.btn_Restart_Click);
+            // 
+            // RVITakePic
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(574, 336);
+            this.Controls.Add(this.btn_Restart);
+            this.Controls.Add(this.tb_Port);
+            this.Controls.Add(this.tb_IP);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lable1);
+            this.Controls.Add(this.lb_UserList);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "RVITakePic";
+            this.Text = "RVITakePic";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lb_UserList;
+        private System.Windows.Forms.Label lable1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tb_IP;
+        private System.Windows.Forms.TextBox tb_Port;
+        private System.Windows.Forms.Button btn_Restart;
+    }
+}
+
---
+++ RVITakePic/RVITakePic.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Collections;
+using prjTCP_ChatRoomServer;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace RVIServer
+{
+    public partial class RVITakePic : Form
+    {
+        TcpListener Server;             //伺服器網路監聽物件 (電話總機)
+        Socket Client;                  //客戶連線物件       (電話分機)
+        Thread Th_Svr;                  //伺服器監聽執行緒   (電話總機開放中))
+        Thread Th_Clt;                  //客戶通話執行緒     (電話分機連線中)
+        Hashtable myHashTable = new Hashtable(); //客戶名稱與通訊物件的集合(key:Name, Socket)
+
+        public RVITakePic()
+        {
+            InitializeComponent();
+            tb_IP.Text = MyIP();
+            using (ReadINI oTINI = new ReadINI(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Config.ini")))
+            {
+                tb_Port.Text = oTINI.getKeyValue("ServerPort", "Value"); //Section name=Worklog；Key name=Value
+            }
+            ServerStart();
+        }
+
+        private string MyIP()
+        {
+            string hostName = Dns.GetHostName();
+            IPAddress[] IPs = Dns.GetHostEntry(hostName).AddressList;
+            foreach (IPAddress item in IPs)
+            {
+                if (item.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    return item.ToString();//如果是IPv4則回傳此字串
+                }
+            }
+            return "";  //找不到合格回傳空字串
+        }
+
+        //建立線上名單
+        private TCPClientData OnlineList()
+        {
+            TCPClientData UpdateUserList= new TCPClientData();
+            string UserList = ""; //代表線上名單的命令碼
+            for (int i = 0; i < lb_UserList.Items.Count; i++)
+            {
+                UserList += lb_UserList.Items[i]; //逐一將成員名單加入L字串
+                if (i < lb_UserList.Items.Count - 1)
+                {
+                    UserList += ",";
+                }
+            }
+            UpdateUserList.UserList = UserList;
+            UpdateUserList.Sender = "Server";
+            UpdateUserList.Target = "ALL";
+            UpdateUserList.Command = "UpdateUserList";
+            return UpdateUserList;
+        }
+
+        private void SendTo(string Str, string User)
+        {
+
+            byte[] B = Encoding.Default.GetBytes(Str);
+            Socket Sck = (Socket)myHashTable[User];
+            Sck.Send(B, 0, B.Length, SocketFlags.None);
+        }
+        private void SendAll(TCPClientData CMDJSON)
+        {
+            string Msg_JSON = JsonConvert.SerializeObject(CMDJSON);
+            byte[] B = Encoding.Default.GetBytes(Msg_JSON);
+            foreach (Socket socket in myHashTable.Values)
+            {
+                socket.Send(B, 0, B.Length, SocketFlags.None);
+            }
+        }
+        //開啟 Server：用 Server Thread 來監聽 Client
+        private void btn_ServerStart_Click(object sender, EventArgs e)
+        {
+            ServerStart();
+        }
+        private void ServerStart()
+        {
+            //忽略跨執行緒處理的錯誤
+            CheckForIllegalCrossThreadCalls = false;
+            Th_Svr = new Thread(ServerSub);
+            Th_Svr.IsBackground = true;
+            Th_Svr.Start();
+        }
+        //接受客戶連線要求的程式(如同電話總機的角色)，針對每一客戶會建立一個連線，以及獨立執行緒
+        private void ServerSub()
+        {
+            //Server IP 和 Port
+            IPEndPoint EP = new IPEndPoint(IPAddress.Parse(tb_IP.Text), int.Parse(tb_Port.Text));
+            Server = new TcpListener(EP);       //建立伺服器端監聽器(總機)
+            Server.Start(100);                  //啟動監聽設定允許最多連線數100人
+            while (true)                        //無限迴圈監聽連線要求
+            {
+                Client = Server.AcceptSocket(); //建立此客戶的連線物件Client
+                Th_Clt = new Thread(Listen);    //建立監聽這個客戶連線的獨立執行緒
+                Th_Clt.IsBackground = true;
+                Th_Clt.Start();
+            }
+        }
+
+        //監聽客戶訊息的程式
+        private void Listen()
+        {
+            Socket Sck = Client;    //複製Client通訊物件到個別客戶專用物件Sck
+            Thread Th = Th_Clt;     //複製執行緒Th_Clt到區域變數Th
+            while (true)            //持續監聽客戶傳來的訊息
+            {
+                //用Sck來接收此客戶訊息，inLen是接收訊息的byte數目
+                try
+                {
+                    byte[] B = new byte[1023];  //建立接收資料用的陣列，長度需大於可能的訊息
+                    int inLen = Sck.Receive(B); //接收網路資訊(byte陣列)
+                    string Msg_JSON = Encoding.Default.GetString(B, 0, inLen);//翻譯實際訊息(長度inLen)
+                    TCPClientData JsonData = JsonConvert.DeserializeObject<TCPClientData>(Msg_JSON);
+
+                    string Cmd = JsonData.Command;    //取出命令碼(第一個|之前的字)
+
+                    switch (Cmd)
+                    {
+                        case "NewUserLogin":               //有新使用者上線:新增使用者到名單中
+                            if (!myHashTable.ContainsKey(JsonData.Sender))
+                            {
+                                myHashTable.Add(JsonData.Sender, Sck);    //連線加入雜湊表，Key:使用者，Valre:連線物件(Socket)
+                                lb_UserList.Items.Add(JsonData.Sender);//加入上線者名單
+                                SendAll(OnlineList());
+                            }
+                            break;
+                        case "UserLogOut":
+                            myHashTable.Remove(JsonData.Sender);    //有使用者離開:從名單中移除該使用者
+                            lb_UserList.Items.Remove(JsonData.Sender);
+                            Th.Abort();
+                            SendAll(OnlineList());
+                            break;
+                        default:
+                            //SendTo(JsonData.Target, Msg_JSON);
+                            SendTo(JsonData.Sender, "拍照完成");
+                            break;
+                    }
+                }
+
+
+                catch (Exception ex)
+                {
+
+                }
+            }
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            using (ReadINI oTINI = new ReadINI("./Config.ini"))
+            {
+                oTINI.setKeyValue("ServerPort", "Value", tb_Port.Text);
+            }
+            Application.ExitThread();
+        }
+
+        private void btn_Restart_Click(object sender, EventArgs e)
+        {
+            Application.Restart();
+            Environment.Exit(0);
+        }
+
+        private void btn_Kick_Click(object sender, EventArgs e)
+        {
+            SendTo("9" + "|" + lb_UserList.SelectedItem.ToString(), lb_UserList.SelectedItem.ToString());
+        }
+        class TCPClientData
+        {
+            public string Command { get; set; }
+            public string DateAndTime { get; set; }
+            public string CarId { get; internal set; }
+            public string Target { get; set; }
+            public string Sender { get; set; }
+            public string UserList { get; set; }
+        }
+    }
+}
